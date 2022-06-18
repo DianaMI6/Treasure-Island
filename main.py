6eb8a7401c2a14828ee425d49fa9d40ccab35ef1
Diana <107622072+DianaMI6@users.noreply.github.com>
2022-06-18 16:44:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -27,10 +27,9 @@ decision1 = input("\n\nYou arrive at Treasure Island!\n\nYou get off the ship an
 
 if decision1 == "jungle":
   decision2 = input("\nAs you venture deeper into the misty woods, the clashing of ocean waves fades. You hear birds singing, wings fluttering and monkeys shrieking.\nYou find a ravine with a gushing stream many meters below. Vines hang down from the thick canopy. To the right you see a narrow path alongside the ravine.\nYou could swing to the other side of the ravine by using one of the vines to your left; or you could take the path to your right.\nWhich way do you want to go? Type 'left' or 'right'.\n").lower()
-  if decision2 == "left":
-    print("\nYou grab a vine and start swinging, but you miscalculated your strength and the wide gap - you fall. Fortunately, you get washed up back ashore on time, just before the sun sets and your ship departs.\nYou leave Treasure Island with empty hands...\n\n\nGAME OVER.")
-  elif decision2 == "right":
-    decision3 = input("\nThe path brings you to a glade with downed trees and dense undergrowth. The glazing sun reveals three colored cave entrances: red, blue and green.\nWhich cave do you want to explore? Type 'red', 'blue' or 'green'.\n").lower()
+  
+  if decision2 == "right":
+    decision3 = input("\nThe path brings you to a glade with downed trees and dense undergrowth. The glazing sun reveals three colored cave entrances: red, blue and green.\nWhich cave do you want to explore? Type 'red', 'blue' or 'green'.\n").lower()    
     if decision3 == "red":
       print("\nYou enter the cave which appears to have blood stains at the walls. You see a pair of glowing, amber eyes looking straight at you from the darkness of the cave. The beast's terrifying growl makes you tremble. You run for your life and miraculously reach the safety of your ship - empty-handed.\nGAME OVER.")
     elif decision3 == "blue":
@@ -43,7 +42,12 @@ if decision1 == "jungle":
 =-=-_=-=-_=-=_=-_= -=======- = =-=_=-=_,-'|"'""-|-,_ 
  =- _=-=-_=- _=-= _--=====- _=-=_-_,-"          |
 jgs=- =- =-= =- = -  -===- -= - ."''')
-  
+    else:
+      print("\n\nYou look to be indecisive. What a pity! You stand and look around, the sun goes down, the ship leaves, and you're still there...\nGAME OVER.")
+      
+  else:
+    print("\nYou grab a vine and start swinging, but you miscalculated your strength and the wide gap - you fall. Fortunately, you get washed up back ashore on time, just before the sun sets and your ship departs.\nYou leave Treasure Island with empty hands...\n\n\nGAME OVER.")
+     
 else:
   print("\nYou walk along the shore. Sunset is near - but you still find no hints for any treasure.\n\nYour crew calls you back to the ship; you leave Treasure Island with empty hands...\n\n\n")
   print('''            ^^                   @@@@@@@@@
